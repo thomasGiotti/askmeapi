8de63d9673390409d5ec12c2128c6b587990fe15
thomas.giotti <tho.giotti@gmail.com>
2022-11-17 17:55:49 +0100

    add config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import * as express from 'express';
-const app = express();
+import * as express from 'express'
+const app = express()
 
-const PORT = 3000;
+const PORT = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
 app.listen(PORT, () => {
-  console.log(`AskMe API is running on port ${PORT}`);
-});
+  console.log(`AskMe API is running on port ${PORT}`)
+})
