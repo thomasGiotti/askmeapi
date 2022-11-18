7d7435cc137b2588f4e6160d108f7799e67e5fc8
thomas.giotti <tho.giotti@gmail.com>
2022-11-18 09:29:23 +0100

    add graphql

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import * as express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import { buildSchema, GraphQLSchema } from 'graphql'
+
+const schema: GraphQLSchema = buildSchema(`type Query {
+  hello : String
+}`)
 
 const app = express()
 
 const PORT = 3000
-
+const root = { hello: () => 'hello world from graphQL' }
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.use('/graphql', graphqlHTTP({ schema, rootValue: root, graphiql: true }))
 // TODO logger
 app.listen(PORT, () => {
-  console.log(`AskMe API is running on port ${PORT}`)
+  console.log(`AskMe API is running on port http://localhost:${PORT}/graphql`)
 })
