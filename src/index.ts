3e50dbe41f213f9db8b9b53dd757316d38a03057
thomas.giotti <tho.giotti@gmail.com>
2022-11-18 13:24:17 +0100

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import { ApolloServer } from 'apollo-server-express'
 import * as express from 'express'
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
 import { createServer } from 'http'
+import * as dotenv from 'dotenv'
 import Schema from './Schemas/Schema'
 import Resolvers from './Resolvers/Resolvers'
+
+dotenv.config()
 
 const startApolloServer = async (schema: any, resolvers: any) => {
   const app = express()
@@ -23,10 +26,10 @@ const startApolloServer = async (schema: any, resolvers: any) => {
 
   await new Promise<void>(
     resolve => {
-      httpServer.listen({ port: 4000 }, resolve)
+      httpServer.listen({ port: process.env.PORT }, resolve)
     } // run the server on port 4000
   )
-  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+  console.log(`Server ready at http://localhost:${process.env.PORT}/${server.graphqlPath}`)
 }
 // in the end, run the server and pass in our Schema and Resolver.
 startApolloServer(Schema, Resolvers)
